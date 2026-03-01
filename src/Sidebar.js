5849704a6bb1c37903d41b100d1515ed21aada3d
Rachel <rachel.sun.rondon@gmail.com>
2026-03-01 14:03:56 -0500

    feat: reset

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,28 +1,9 @@
-import { useState } from 'react';
-import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
 import './Sidebar.css';
 
-const Sidebar = () => {
-
-  const [theme, setTheme] = useState(true);
-  const className = `layout ${theme}`;
-
-  const toggle = () => {
-    setTheme(!theme);
-  };
-
-  console.log(theme);
+const Toggle = () => {
 
   return (
-    <div className={className}>
-      <div className="toggle">
-        {theme ? (
-          <MoonIcon className="icon moon" value="false" onClick={toggle} />  
-        ):
-          <SunIcon className="icon sun" value="true" onClick={toggle}/>
-          }
-      </div>
-     </div>
+    <div></div>
   )};
 
-export default Sidebar;
+export default Toggle;
