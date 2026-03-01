ae775426577c84c9313104472a93c167a044c435
Rachel <rachel.sun.rondon@gmail.com>
2026-03-01 17:17:58 -0500

    feat: demo

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
 import './Toggle.css';
 
 const Toggle = () => {
 
+  const [toggled, setToggled] = useState(false);
+  const className = `layout ${toggled}`;
+
+  const toggle = () => {
+    setToggled(!toggled);
+  };
+
 return (
-  <div></div>
+  <div className={className}>
+    <div className="toggle">
+      {toggled ? (
+        <SunIcon className="icon" onClick={toggle} />
+      ) :
+        <MoonIcon className="icon" onClick={toggle} />
+      }
+    </div>
+  </div>
 )};
 
 
