66bfb054aed90881f7592820018ed021210920c6
Rachel <rachel.sun.rondon@gmail.com>
2026-03-01 14:43:15 -0500

    feat: final layout + styles

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,18 +2,14 @@ import { useState } from 'react';
 import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
 import './Toggle.css';
 
-
 const Toggle = () => {
-
 
 const [toggled, setToggled] = useState(false);
 const className = `layout ${toggled}`;
 
-
 const toggle = () => {
   setToggled(!toggled);
 };
-
 
 return (
   <div className={className}>
