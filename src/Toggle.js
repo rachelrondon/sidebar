dade486f88cf09e7f2cdf932ea106be06605ac8b
Rachel <rachel.sun.rondon@gmail.com>
2026-03-01 14:12:37 -0500

    feat: reset

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,15 +1,9 @@
-import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
 import './Toggle.css';
 
 const Toggle = () => {
 
   return (
-    <div>
-      <div className="toggle">
-        <SunIcon />
-        <MoonIcon />
-      </div>
-    </div>
+    <div></div>
   )};
 
 export default Toggle;
